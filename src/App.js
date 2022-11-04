e19728efb22a4b3eecdd0e3ded0f4182882857ad
DavitMkhitaryan <davitmkhitaryanit@gmail.com>
2022-11-04 15:33:30 +0400

    Finalize UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
 
+  const [search, setSearch] = useState("");
+
   const [allData, setAllData] = useState({
     city: "",
     country: "",
-    temperature: ""
+    temperature: "",
+    humidity: "",
+    temp_min: "",
+    weather_icon: ""
   });
 
   useEffect(() => {
@@ -18,29 +22,62 @@ function App() {
   const fetchData = async (city) => {
     try {
       const APIKEY = "YOUR_API_KEY";
-      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${"milan"}&appid=${APIKEY}&units=metric`);
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`);
       await setAllData({
         city: result.data.name,
         country: result.data.sys.country,
-        temperature: result.data.main.temp
+        temperature: result.data.main.temp,
+        humidity: result.data.main.humidity,
+        temp_min: result.data.main.temp_min,
+        weather_icon: result.data.weather[0]["icon"]
       });
     } catch (e) {
       console.log("API not loaded correctly or loaded the first time");
     }
   }
 
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+    console.log(search);
+  }
+
+  const handleSubmit = (event) => {
+    console.log(search);
+    event.preventDefault();
+    fetchData(search);
+  }
+
   return (
     <main>
-      <div className="App">
-        {console.log("1. test for: successful deployment", 
-        "2. test for: fetching weather data API (country, city, weather)", 
-        allData.country, allData.city, allData.temperature)}
-
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="city" placeholder="Location" value={search} onChange={handleChange} />
+          <button for="city">Search</button>
+        </form>
         <section>
-          <h1>{allData.city}</h1>
-          <h2>{allData.country}</h2>
-          <h3>TEMPERATURE:</h3>
-          <p>{allData.temperature}°</p>
+          <div className='header-div'>
+            <div>
+              <div className='data'>
+                <img src={`http://openweathermap.org/img/w/${allData.weather_icon}.png`} alt="weather icon" />
+                <h1 className='title'>{allData.city}</h1>
+                <h2 className='location'>{allData.country}</h2>
+                <div className='weather-description'>
+                  <div>
+                    <h3>TEMPERATURE:</h3>
+                    <p>{allData.temperature}°C</p>
+                  </div>
+                  <div>
+                    <h3>HUMIDITY:</h3>
+                    <p>{allData.humidity}%</p>
+                  </div>
+                  <div>
+                    <h3>MINIMUM TEMPERATURE:</h3>
+                    <p>{allData.temp_min}°C</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </main>
